bd2c9f763a467bac483fcae7b2b5eca7bd126b3c
xiaowei <578850593@qq.com>
2019-01-21 08:48:12 +0800

    延迟删除

--- src/components/begin_left.vue
+++ src/components/begin_left.vue
@@ -8,7 +8,7 @@
     <div class="left_content">
       <div class="left_content_bg">
         <!-- <img :src="`${ list.prizePicture }`" :style="{width: `${firstPWidth}%`}"> -->
-        <!-- <img :src="'http://10.12.0.62:8082/' + `${ list.prizePicture }`" :style="{width: `${firstPWidth}%`}"> -->
+        <!-- <img :src="'https://10.12.0.62:8082/' + `${ list.prizePicture }`" :style="{width: `${firstPWidth}%`}"> -->
         <img :src="'http://10.11.8.207:8099/' + `${ list.prizePicture }`" :style="{width: `${firstPWidth}%`}">
 
       </div>
@@ -96,9 +96,9 @@ export default {
           if(res.status === 1) {
             this.setAwardContent(false)
             this.setBeginLock(true)
-            setTimeout(() => {
+            // setTimeout(() => {
               this.setAwardList(res.list)
-            },1000)
+            // },500)
             this.allNum = this.allNum - this.drawNum
             console.log('meiyoujia allNum', this.allNum)
             this.num = 1
